a868cad59c3b7d955dd32779a8739d1b5fc4e42e
dh7548 <dh7548@ship.edu>
2020-11-27 12:04:28 -0500

    fixed testServerState constructor

--- MenuStateMachine/src/TestServerState.java
+++ MenuStateMachine/src/TestServerState.java
@@ -2,17 +2,18 @@
  * 
  * @author Dan Holmgren
  *
- * This class provides the menu for testing the servers
+ *         This class provides the menu for testing the servers
  */
 public class TestServerState extends State {
 
   /**
    * 
    */
-  TestServerState(){
-    MenuOption[] myMenuOptions = 
-      { new MenuOptionForMenu("Go Back", MenuStateEnum.START_MENU),
-          new MenuOptionForAction("Test the servers", new TestServersAction()),
-          new MenuOptionForMenu("Exit", MenuStateEnum.END_STATE)};
-      }
+  TestServerState() {
+    MenuOption[] myMenuOptions = { new MenuOptionForMenu("Go Back", MenuStateEnum.START_MENU),
+        new MenuOptionForAction("Test the servers", new TestServersAction()),
+        new MenuOptionForMenu("Exit", MenuStateEnum.END_STATE) };
+
+    super.loadMenu(myMenuOptions);
+  }
 }
