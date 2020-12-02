2341e5dc6b0e7894c71046eb46f563ea8bfe0e66
dh7548 <dh7548@ship.edu>
2020-12-02 13:02:10 -0500

    whoops

---
+++ MenuStateMachine/src/PingServerAction.java
@@ -0,0 +1,9 @@
+
+public class PingServerAction extends MenuAction{
+
+  @Override
+  void execute() {
+    System.out.println("This will eventually ping the servers to make sure connections are working or something");
+  }
+
+}
